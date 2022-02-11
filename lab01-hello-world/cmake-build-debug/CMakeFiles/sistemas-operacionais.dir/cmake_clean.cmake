bf7fc616b5286e47f3d9a3f8a0071d1104b35068
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2022-02-10 22:34:58 -0300

    Organiza estrutura de pastas do projeto
    Signed-off-by: jenifer-mathias <jenifer.mathias.santos@hotmail.com>

---
+++ lab01-hello-world/cmake-build-debug/CMakeFiles/sistemas-operacionais.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sistemas-operacionais.dir/HelloWorld.cpp.o"
+  "sistemas-operacionais"
+  "sistemas-operacionais.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sistemas-operacionais.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
