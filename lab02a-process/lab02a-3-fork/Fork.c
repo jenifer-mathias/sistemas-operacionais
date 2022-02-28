31cc7c92e39b5baaae435be841b3a5dff1a8f03f
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2022-02-28 20:18:02 -0300

    adds examples of process creation and communication
    Signed-off-by: jenifer-mathias <jenifer.mathias.santos@hotmail.com>

---
+++ lab02a-process/lab02a-3-fork/Fork.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(void) {
+    pid_t childPid;
+    childPid = fork();
+    if (childPid == -1) {
+        perror("Failed to fork");
+        return 1;
+    }
+    if (childPid == 0)                                         /** child code */
+        printf("\n I am child %ld\n", (long)getpid());
+    else                                                      /** parent code */
+        printf("\n I am parent %ld\n", (long)getpid());
+    return 0;
+}
