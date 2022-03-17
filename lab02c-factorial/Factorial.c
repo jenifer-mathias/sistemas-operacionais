2aad61a5b71dd40e10c3f3f8756771c7cef6162e
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2022-03-17 18:41:24 -0300

    Add file to Factorial
    Signed-off-by: jenifer-mathias <jenifer.mathias.santos@hotmail.com>

---
+++ lab02c-factorial/Factorial.c
@@ -0,0 +1,24 @@
+/** Considere o trecho de código em anexo para resolver o seguinte item:
+1) Transforme o código em um exemplo multiprocessado
+
+# include <stdio.h>
+
+int main() {
+    int i, num, j;
+    printf ("Enter the number: ");
+    scanf ("%d", &num );
+
+	j = 1;
+	for (i = 1; i<=num; i++) {
+        j=j*i;
+	}
+
+    printf("The factorial of %d is %d\n",num,j);
+}
+ **/
+
+#include <stdio.h>
+
+int main() {
+
+}
---
+++ lab02c-factorial/README.md
@@ -0,0 +1,6 @@
+# Lab 02-c Factorial
+
+**Resultado da execução: **
+
+![image](https://github.com/jenifer-mathias/sistemas-operacionais/blob/main/assets/factorial.png)
+
