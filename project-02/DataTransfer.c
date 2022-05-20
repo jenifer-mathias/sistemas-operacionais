1accab6f4085d6aec795762be27cd02e10a012d8
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2022-05-19 23:04:50 -0300

    first attempt to resolve account transfer code
    Signed-off-by: jenifer-mathias <jenifer.mathias.santos@hotmail.com>

--- project-02/DataTransfer.c
+++ project-02/DataTransfer.c
@@ -1,60 +1,53 @@
-#define _GNU_SOURCE
-#include <stdlib.h>
-#include <malloc.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <sched.h>
+#include <pthread.h>
 #include <stdio.h>
-// 64kB stack
-#define FIBER_STACK 1024*64
-struct c {
- int saldo;
+#include <stdlib.h>
+
+# define NUMBER_OF_THREADS 2 /** definição das threads */
+
+struct Conta {
+    int saldo;
 };
-typedef struct c conta;
-conta from, to;
+
+typedef struct Conta conta;
+conta contaDe, contaPara;
 int valor;
-// The child thread will execute this function
-int transferencia( void *arg)
-{
- if (from.saldo >= valor){ // 2
- from.saldo -= valor;
- to.saldo += valor;
- }
- printf("Transferência concluída com sucesso!\n");
- printf("Saldo de c1: %d\n", from.saldo);
- printf("Saldo de c2: %d\n", to.saldo);
- return 0;
+
+int transferencia() {
+    if (contaDe.saldo >= valor) {
+        contaDe.saldo -= valor;
+        contaPara.saldo += valor;
+    }
+
+    printf("\nTransferência concluída com sucesso!\n");
+    printf("\nSaldo da conta de envio (C1): %d\n", contaDe.saldo);
+    printf("\nSaldo da conta de destino (C2): %d\n", contaPara.saldo);
+
+    pthread_exit(NULL);
 }
-int main()
-{
- void* stack;
- pid_t pid;
- int i;
- // Allocate the stack
- stack = malloc( FIBER_STACK );
- if ( stack == 0 )
- {
- perror("malloc: could not allocate stack");
- exit(1);
- }
- // Todas as contas começam com saldo 100
-from.saldo = 100;
- to.saldo = 100;
- printf( "Transferindo 10 para a conta c2\n" );
- valor = 10;
- for (i = 0; i < 10; i++) {
- // Call the clone system call to create the child thread
- pid = clone( &transferencia, (char*) stack + FIBER_STACK,
- SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, 0 );
- if ( pid == -1 )
- {
- perror( "clone" );
- exit(2);
- }
- }
- // Free the stack
- free( stack );
- printf("Transferências concluídas e memória liberada.\n");
- return 0;
+
+void tranferenciaEntreContas(pthread_t threads[NUMBER_OF_THREADS]) {
+    valor = 10;
+
+    /** Todas as contas começam com saldo 100 */
+
+    contaDe.saldo = 100;
+    contaPara.saldo = 100;
+    printf("\nSaldo das contas inicialmente: \n");
+    printf("\nSaldo da conta de envio (C1): %d\n", contaDe.saldo);
+    printf("\nSaldo da conta de destino (C2): %d\n", contaPara.saldo);
+
+
+    for (int i = 0; i < valor; i++) {
+        printf( "\nTransferindo 10 para a conta conta de destino:\n" );
+        pthread_create(&threads[i], NULL, transferencia, i);
+        contaDe.saldo;
+        contaPara.saldo;
+    }
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t threads[NUMBER_OF_THREADS]; /** cria duas threads (uma para cada conta) **/
+    tranferenciaEntreContas(threads);
+    EXIT_SUCCESS;
 }
