00167be51eda71156597e89e80f322a6fcb1ca45
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2022-05-29 21:33:18 -0300

    add method to manage the threads and start the program
    Signed-off-by: jenifer-mathias <jenifer.mathias.santos@hotmail.com>

--- project-02/DataTransfer.c
+++ project-02/DataTransfer.c
@@ -49,3 +49,36 @@ void *recebeDaConta(void *ptr) {
     }
     pthread_exit(0);
 }
+
+void gerenciaThreads() {
+    pthread_t pro, con; /** cria duas threads (uma para cada conta) **/
+    pthread_mutex_init(&the_mutex, 0);
+
+    pthread_cond_init(&condc, 0);
+    pthread_cond_init(&condp, 0);
+
+    pthread_create(&con, 0, transfereParaOutraConta(pro), 0);
+    pthread_create(&con, 0, recebeDaConta(con), 0);
+
+    pthread_join(pro, 0);
+    pthread_join(con, 0);
+
+    pthread_cond_destroy(&condc);
+    pthread_cond_destroy(&condp);
+
+    pthread_mutex_destroy(&the_mutex);
+}
+
+int main() {
+    /**  Todas as contas começam com saldo 100 */
+    contaDe.saldo = 100;
+    contaPara.saldo = 100;
+
+    printf("\nSaldo das contas inicialmente: \n");
+    printf("\nSaldo da conta de envio (C1): %d\n", contaDe.saldo);
+    printf("\nSaldo da conta de destino (C2): %d\n", contaPara.saldo);
+
+    gerenciaThreads();
+
+    EXIT_SUCCESS;
+}
